8ebe202a6064b09d8d39da28652a7a1e26fafd94
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-18 20:49:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
-class Dog 
-  def name= (Flank)
-    this_dogs_name = Flank
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
-
-def name 
-  this_dogs_name
+ 
+  def name
+    @this_dogs_name
+  end
 end
-end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+ 
